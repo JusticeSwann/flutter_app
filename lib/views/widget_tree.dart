1cc57d1f056c235557695a7ebfd2e2c39c178594
JusticeSwann <90804100+JusticeSwann@users.noreply.github.com>
2025-01-27 11:10:11 -0400

    reached 1:32:48 in video

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/data/notifiers.dart';
 import 'package:flutter_application/views/pages/home_page.dart';
 import 'package:flutter_application/views/pages/profile_page.dart';
 import 'package:flutter_application/widgets/bottom_navbar.dart';
@@ -8,18 +9,23 @@ List<Widget> pages = [
   ProfilePage(),
 ];
 
+
 class WidgetTree extends StatelessWidget{
   const WidgetTree({super.key});
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter_Map'),
-          centerTitle: true,
-        ),
-        body: pages.elementAt(0),
-        bottomNavigationBar:BottomNavbarWidget(),
-      );
+      appBar: AppBar(
+        title: const Text('Flutter_Map'),
+        centerTitle: true,
+      ),
+      body: ValueListenableBuilder(valueListenable: selectedPageNotifier,
+        builder: (context, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar:BottomNavbarWidget(),
+    );
   }
 }
--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/data/notifiers.dart';
 
-class BottomNavbarWidget extends StatefulWidget{
+class BottomNavbarWidget extends StatelessWidget{
   const BottomNavbarWidget({super.key});
 
   @override
-  State<BottomNavbarWidget> createState() => _BottomNavBarWidgetState();
-}
-
-class _BottomNavBarWidgetState extends State<BottomNavbarWidget>{
-  int selectedIndex = 0;
-  @override
 
   Widget build(BuildContext context){
-    return NavigationBar(
-      destinations: [
-        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-        NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-      ],
-      onDestinationSelected: (int value) {
-        setState(() {
-          selectedIndex = value;
-        });
+    return ValueListenableBuilder(valueListenable: selectedPageNotifier, 
+      builder: (context, selectedPage, child) {
+        return NavigationBar(
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+          onDestinationSelected: (int value) {
+            selectedPageNotifier.value = value;
+          },
+          selectedIndex: selectedPage,
+        );
       },
-      selectedIndex: selectedIndex,
     );
   }
 }
